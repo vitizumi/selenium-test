f44b6c82bac3c8ca1539579be3159f50ebcd0a6c
vitizumi <vitor.r.izumi@gmail.com>
2025-03-24 23:57:45 -0300

    Add fluent Wait test

--- pageobjecttest/src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
+++ pageobjecttest/src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
@@ -10,6 +10,7 @@ public class WidgetsPage extends HomePage {
 
     private By selectMenuMenuItem = By.xpath("//li[@id='item-8']/span[text()='Select Menu']");
     private By selectDatePickerItem = By.xpath("//li[@id='item-2']/span[text()='Date Picker']");
+    private By progressBarMenuItem = By.xpath("//li[@id='item-4']/span[text()='Progress Bar']");
 
     public SelectMenuPage clickSelectMenu() {
         scrollToElementJS(selectMenuMenuItem);
@@ -21,5 +22,11 @@ public class WidgetsPage extends HomePage {
         scrollToElementJS(selectDatePickerItem);
         click(selectDatePickerItem);
         return new DatePickerPage();
+    }
+
+    public ProgressBarPage clickProgressBarMenu(){
+        scrollToElementJS(progressBarMenuItem);
+        click(progressBarMenuItem);
+        return new ProgressBarPage();
     }
 }
--- pageobjecttest/src/test/java/part3_4/com/demoqa/test/part4/dynamicwait/DynamicWaitTest.java
+++ pageobjecttest/src/test/java/part3_4/com/demoqa/test/part4/dynamicwait/DynamicWaitTest.java
@@ -7,12 +7,22 @@ import part3_4.com.demoqa.base.BaseTest;
 
 public class DynamicWaitTest extends BaseTest {
 
-    @Test
+
     public void testVisibleAfterButtonText() {
         var dynamicPage = homePage.goToElements().clickDynamicProperties();
         String actualText = dynamicPage.getVisibleAfterButtonText();
         String expectedText = "Visible After 5 Seconds";
         Assert.assertEquals(actualText, expectedText, "Actual and Expected text do not match.");
+    }
+
+    @Test
+    public void testProgressBar() {
+        var progressBarPage = homePage.goToWidgets().clickProgressBarMenu();
+        progressBarPage.clickStartButton();
+        String actualValue = progressBarPage.getProgressValue();
+        String expectedValue = "100%";
+        Assert.assertEquals(actualValue, expectedValue, "Progress Bar is not at 100%.");
+        
     }
 
 }
